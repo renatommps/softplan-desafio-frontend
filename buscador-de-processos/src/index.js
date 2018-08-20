6b62d8b9687d046842c5f81c7cc0997f35da4833
DESKTOP-JI1N0GH\Renato <renatommps@gmail.com>
2018-08-20 18:19:28 -0300

    Atualizado index.js para  criar a store da aplicação e utulizar rotas.

--- buscador-de-processos/src/index.js
+++ buscador-de-processos/src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ProcessListView from './containers/process/ProcessListView';
+import allReducers from './reducers/Reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    allReducers,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact={true} component={App} />
+                <Route path="/processos" exact={true} component={ProcessListView} />
+                <Redirect from="*" to="/"/>
+            </Switch>
+        </ BrowserRouter>
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
