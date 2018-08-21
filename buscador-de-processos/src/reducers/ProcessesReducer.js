fb39f43dca6da11e47da9370b779ff6ae603e3de
DESKTOP-JI1N0GH\Renato <renatommps@gmail.com>
2018-08-21 16:34:07 -0300

    Adicionada action de deleção de processo no ProcessesReducer

--- buscador-de-processos/src/reducers/ProcessesReducer.js
+++ buscador-de-processos/src/reducers/ProcessesReducer.js
@@ -2,8 +2,10 @@ export default function(state = null, action) {
     switch(action.type) {
         case 'PREOCESSES_FETCHED':
             return { ...state, data: action.payload };
-            case 'PROCESS_SUBMISSION_SUCESS':
-            return { ...state, data: action.payload };
+        case 'PROCESS_SUBMISSION_SUCESS':
+            return { ...state};
+        case 'PREOCESSES_DELETED_SUCESS':
+            return { ...state, data: state.data.filter(process => process.id !== action.payload.id)};
         default:
             return state;
     }
