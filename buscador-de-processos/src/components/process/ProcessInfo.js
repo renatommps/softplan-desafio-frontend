32ba54d45d8ec18219031216bd5df38b40350dc7
DESKTOP-JI1N0GH\Renato <renatommps@gmail.com>
2018-08-22 11:45:09 -0300

    Ajustada formatação do card de informações detalhadas dos processos

--- buscador-de-processos/src/components/process/ProcessInfo.js
+++ buscador-de-processos/src/components/process/ProcessInfo.js
@@ -1,18 +1,15 @@
 import React,{ Component } from  'react'
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 class ProcessInfo extends Component {
-    
-    handleProcessSearchTerm = (process) => {
-        this.props.getProcessBySearchTerm(process);
-        this.context.router.history.push("/processos");
-    }
     
     render() {
         if (!this.props.process) {
@@ -20,27 +17,27 @@ class ProcessInfo extends Component {
         }
         
         const { numero, entrada, interessados, descricao, assunto } = this.props.process;
-        const { classes } = this.props;
+        const { classes, onClose } = this.props;
 
         return (
-            <Paper>
-                <Grid container spacing={8} direction="column">
+            <Paper className={classes.paper} elevation={1}>
+                <Grid container spacing={16} direction="column">
 
                     {/* HEADER */}
-                    <Grid container item xs={12} spacing={8} direction="row">
+                    <Grid container item xs={12} spacing={8} direction="row" justify="space-between">
 
                         {/* IMAGEM */}
                         <Grid container item xs={2} spacing={8}>
-                            <div>IMAGEM</div>
+                            <div className={classes.square}/>
                         </Grid>
 
                         {/* CONTEUDO */}
                         <Grid container item xs={10} spacing={8} direction="row">
 
                             {/* PRIMEIRA LINHA */}
-                            <Grid container item xs={12} spacing={8} direction="row">
+                            <Grid container item xs={12} spacing={0} direction="row" justify="space-between">
                                 {/* PRIMEIRA COLUNA DA PRIMRA LINHA */}
-                                <Grid container item xs={6} spacing={8} direction="column">
+                                <Grid container item xs={6} spacing={0} direction="column">
                                     <Typography className={classes.gridItemText} color="textSecondary">
                                         Processo
                                     </Typography>
@@ -49,7 +46,7 @@ class ProcessInfo extends Component {
                                     </Typography>
                                 </Grid>
                                 {/* SEGUNDA COLUNA DA PRIMRA LINHA */}
-                                <Grid container item xs={5} spacing={8} direction="column">
+                                <Grid container item xs={5} spacing={0} direction="column">
                                     <Typography className={classes.gridItemText} color="textSecondary">
                                         Data
                                     </Typography>
@@ -58,13 +55,17 @@ class ProcessInfo extends Component {
                                     </Typography>
                                 </Grid>
                                 {/* TERCEIRA COLUNA DA PRIMRA LINHA */}
-                                <Grid container item xs={1} spacing={8} direction="column">
-                                    <div>XIS</div>
+                                <Grid container item xs={1} spacing={0} direction="row" justify="flex-start">
+                                    <Grid container item xs={12} justify="flex-start">
+                                        <IconButton onClick={onClose} className={classes.closeButton}>
+                                            <CloseIcon />
+                                        </IconButton>
+                                    </Grid>
                                 </Grid>
                             </Grid>
 
                             {/* SEGUNDA LINHA */}
-                            <Grid container item xs={12} spacing={8} direction="row">
+                            <Grid container item xs={12} spacing={0} direction="column" justify="space-between">
                                 <Typography className={classes.gridItemText} color="textSecondary">
                                     Assunto
                                 </Typography>
@@ -76,7 +77,7 @@ class ProcessInfo extends Component {
                     </Grid>
 
                     {/* INTERESSADOS */}
-                    <Grid container item xs={12} spacing={8} direction="column">
+                    <Grid container item xs={12} spacing={8} direction="column" justify="space-between">
                         <Typography className={classes.gridItemText} color="textSecondary">
                             Interessados
                         </Typography>
@@ -86,27 +87,27 @@ class ProcessInfo extends Component {
                     </Grid>
 
                     {/* DESCRICAO */}
-                    <Grid container item xs={12} spacing={8} direction="column">
+                    <Grid container item xs={12} spacing={8} direction="column" justify="space-between">
                         <Typography className={classes.gridItemText} color="textSecondary">
                             Descrição
                         </Typography>
-                        <Typography className={classes.gridItemText} component="p">
+                        <Typography component="p" className={classes.textWithParagraphy}>
                             {descricao}
                         </Typography>
                     </Grid>
 
                     {/* BOTOES */}
-                    <Grid container item xs={12} spacing={8} direction="row">
-                        <Paper>
-                            <Link to="#" onClick={this.props.deleteProcess}>
+                    <Grid container item xs={12} spacing={16} direction="row" justify="flex-end">
+                        <Grid item>
+                            <Button variant="outlined" className={"app-box-shadow button-label black-54"} onClick={this.props.deleteProcess}>
                                 REMOVER
-                            </Link>
-                        </Paper>
-                        <Paper>
-                            <Link to="#">
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button variant="outlined" disabled className={"app-box-shadow button-label black-54"}>
                                 EDITAR
-                            </Link>
-                        </Paper>
+                            </Button>
+                        </Grid>
                     </Grid>
 
                 </Grid>
@@ -116,6 +117,45 @@ class ProcessInfo extends Component {
 }
 
 const styles = {
+    paper: {
+        padding: 16
+    },
+    textWithParagraphy: {
+        whiteSpace: "pre-line",
+    },
+    square: {
+        position: "relative",
+        width: "100%",
+        paddingBottom: "100%",
+        height: 0,
+        background: "#9E9E9E",
+        "&::before": {
+            position: "absolute",
+            content: "''",
+            width: "100%",
+            height: "1px",
+            bottom: "50%",
+            backgroundColor: "white",
+            transform: "rotate(45deg)",
+        },
+        "&::after": {
+            position: "absolute",
+            content: "''",
+            width: "100%",
+            height: "1px",
+            bottom: "50%",
+            backgroundColor: "white",
+            transform: "rotate(-45deg)",
+        },
+    },
+    crossLines: {
+        position: "absolute",
+        width: "100%",
+        paddingBottom: "100%",
+        border: "1px solid white",
+        height: 0,
+        background: "rgba(0, 0, 0, 0.0)",
+    },
     cardRoot: {
         padding: 0,
     },
@@ -131,6 +171,7 @@ const styles = {
         overflow: 'hidden',
         whiteSpace: 'nowrap',
         minWidth: 0,
+        padding: 0,
     },
     selected: {
         borderColor: '#005b95',
@@ -139,7 +180,7 @@ const styles = {
     },
     cardAction: {
         padding: 0,
-    },
+    }
 };
 
 ProcessInfo.propTypes = {
